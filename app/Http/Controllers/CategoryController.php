e6c86d805982664304cc15c2d6cbf47ab7e6e827
[AndreaS] <solfa_90@hotmail.it>
2021-06-23 15:23:28 +0200

    insert category data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreCategoryRequest;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends Controller
 {
@@ -15,10 +16,16 @@ class CategoryController extends Controller
 
     public function store(StoreCategoryRequest $request)
     {
-        /* $category = new Category();
+        $category = new Category;
         $category->category_name = $request->input('category_name');
+        $category->user_id = Auth::user()->id;
         $category->save();
+        /*   Category::insert([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id,
+            'created_at' => now(),
+        ]); */
 
-         */
+        return redirect()->back()->with('success', 'Category inserted successfully!');
     }
 }
